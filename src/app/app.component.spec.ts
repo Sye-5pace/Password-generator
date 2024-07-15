b913ca5f5b69768a01e547826012ce2085b0a4a2
Silas Owu <owusilasyp00@gmail.com>
2024-07-15 09:59:49 -0700

    test: write intial test suite for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { PasswordViewComponent } from './components/password-view/password-view.component';
-import { GeneratorSettingsComponent } from './components/generator-settings/generator-settings.component';
-import { By } from '@angular/platform-browser';
-
 
 describe('AppComponent', () => {
-  let component: AppComponent;
-  let fixture: ComponentFixture<AppComponent>
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent,PasswordViewComponent, GeneratorSettingsComponent],
+      imports: [AppComponent],
     }).compileComponents();
   });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  })
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -27,14 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render the PasswordViewComponent', () => {
-    const passwordViewDebugElement = fixture.debugElement.query(By.directive(PasswordViewComponent));
-    expect(passwordViewDebugElement).not.toBeNull();
+  it(`should have the 'password-generator' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('password-generator');
   });
 
-  it('should render the GeneratorSettingsComponent', () => {
-    const generatorSettingsDebugElement = fixture.debugElement.query(By.directive(GeneratorSettingsComponent));
-    expect(generatorSettingsDebugElement).not.toBeNull();
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, password-generator');
   });
-
 });
