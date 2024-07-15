b8428cb7ea5fdbde4968e3a97d0ab0fc5e8cf82c
Silas Owu <owusilasyp00@gmail.com>
2024-07-15 10:22:48 -0700

    chore: update devdependencies

--- src/app/components/password-view/password-view.component.ts
+++ src/app/components/password-view/password-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Clipboard, ClipboardModule } from '@angular/cdk/clipboard';
 
 
 @Component({
   selector: 'app-password-view',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,ClipboardModule],
   templateUrl: './password-view.component.html',
   styleUrl: './password-view.component.scss'
 })
@@ -14,6 +15,6 @@ export class PasswordViewComponent {
   password: string= 'P4$5W0rD!';
 
   copyText() {
-    
+
   }
 }
