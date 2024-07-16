6036dfae3b8240de075607d189acc2a590c7ee96
Silas Owu <owusilasyp00@gmail.com>
2024-07-16 03:01:01 -0700

    style: update ui

--- src/app/components/generator-settings/generator-settings.component.ts
+++ src/app/components/generator-settings/generator-settings.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-generator-settings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './generator-settings.component.html',
   styleUrl: './generator-settings.component.scss'
 })
-export class GeneratorSettingsComponent {
 
+export class GeneratorSettingsComponent {
+  charLength: number = 10;
+  progressBar: number = 10;
 }
